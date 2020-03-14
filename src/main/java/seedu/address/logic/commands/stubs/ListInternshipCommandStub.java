8902e0dc603243dfcea838667f8eb4c1e4db3e20
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2020-03-14 12:56:22 +0800

    Add stubs for list command:

---
+++ src/main/java/seedu/address/logic/commands/stubs/ListInternshipCommandStub.java
@@ -0,0 +1,9 @@
+package seedu.address.logic.commands.stubs;
+
+import seedu.address.logic.commands.ListCommand;
+
+public class ListInternshipCommandStub extends ListCommand {
+    public ListInternshipCommandStub() {
+
+    }
+}
---
+++ src/main/java/seedu/address/logic/commands/stubs/ListResumeCommandStub.java
@@ -0,0 +1,9 @@
+package seedu.address.logic.commands.stubs;
+
+import seedu.address.logic.commands.ListCommand;
+
+public class ListResumeCommandStub extends ListCommand {
+    public ListResumeCommandStub() {
+
+    }
+}
