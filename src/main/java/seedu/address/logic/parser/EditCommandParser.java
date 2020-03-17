ef8c644906a82d85d3dea447616caad23dee6297
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2020-03-17 11:31:14 +0800

    Handle EditCommand parsing for Internship item

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -2,9 +2,13 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FROM;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ITEM;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TO;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -13,9 +17,12 @@ import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.edit.EditCommand;
+import seedu.address.logic.commands.edit.EditInternshipCommand;
+import seedu.address.logic.commands.edit.EditInternshipDescriptor;
 import seedu.address.logic.commands.edit.EditResumeCommand;
 import seedu.address.logic.commands.edit.EditResumeDescriptor;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.item.Item;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -31,14 +38,20 @@ public class EditCommandParser implements Parser<EditCommand> {
     public EditCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_TAG, PREFIX_ITEM);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_TAG, PREFIX_ITEM, PREFIX_FROM, PREFIX_TO,
+                        PREFIX_ROLE, PREFIX_DESCRIPTION);
 
         Index index;
 
+        // TODO: Better error handling
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
+        }
+
+        if (!argMultimap.getValue(PREFIX_ITEM).isPresent()) {
+            throw new ParseException(Item.MESSAGE_CONSTRAINTS);
         }
 
         String itemType = ParserUtil.parseItemType(argMultimap.getValue(PREFIX_ITEM).get());
@@ -47,6 +60,7 @@ public class EditCommandParser implements Parser<EditCommand> {
         case "res":
             EditResumeDescriptor editResumeDescriptor = new EditResumeDescriptor();
             // TODO: Not sure if I should create a method inside the respective descriptors for this checking.
+            // Considerations: If I add a method inside the descriptor, then potentially need dependencies
             if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
                 editResumeDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
             }
@@ -57,6 +71,29 @@ public class EditCommandParser implements Parser<EditCommand> {
             }
 
             return new EditResumeCommand(index, editResumeDescriptor);
+        case "int":
+            EditInternshipDescriptor editInternshipDescriptor = new EditInternshipDescriptor();
+            if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
+                editInternshipDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
+            }
+            if (argMultimap.getValue(PREFIX_ROLE).isPresent()) {
+                editInternshipDescriptor.setRole(argMultimap.getValue(PREFIX_ROLE).get().trim());
+            }
+            if (argMultimap.getValue(PREFIX_FROM).isPresent()) {
+                editInternshipDescriptor.setFrom(argMultimap.getValue(PREFIX_FROM).get().trim());
+            }
+            if (argMultimap.getValue(PREFIX_TO).isPresent()) {
+                editInternshipDescriptor.setTo(argMultimap.getValue(PREFIX_TO).get().trim());
+            }
+            if (argMultimap.getValue(PREFIX_DESCRIPTION).isPresent()) {
+                editInternshipDescriptor.setDescription(argMultimap.getValue(PREFIX_DESCRIPTION).get().trim());
+            }
+
+            if (!editInternshipDescriptor.isAnyFieldEdited()) {
+                throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
+            }
+
+            return new EditInternshipCommand(index, editInternshipDescriptor);
         default:
             // Should not have reached here
             // TODO: Use a better Exception here
