9b089a838992bf6004e3bf6c0eb28ed33fc4406a
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-03-19 15:58:27 +0800

    Add functionality for View command

--- src/main/java/seedu/address/logic/commands/view/ViewInternshipCommand.java
+++ src/main/java/seedu/address/logic/commands/view/ViewInternshipCommand.java
@@ -1,21 +1,42 @@
 package seedu.address.logic.commands.view;
 
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.item.Internship;
 
 /**
  * The command for viewing the details of an Internship item.
  */
 public class ViewInternshipCommand extends ViewCommand {
-    private Index targetIndex;
 
     public ViewInternshipCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
+        super(targetIndex);
     }
 
     @Override
-    public CommandResult execute(Model model) {
-        return null;
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (targetIndex.getZeroBased() >= model.getInternshipSize()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Internship toView = model.getInternship(targetIndex);
+        model.setInternshipToDisplay();
+
+        return new CommandResult(toView.toString(),
+                String.format(MESSAGE_VIEW_SUCCESS, toView));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ViewInternshipCommand // instanceof handles nulls
+                && targetIndex.equals(((ViewInternshipCommand) other).targetIndex)); // state check
     }
 }
