5848a40c284b598274995bf7dbf00e6f73838bcf
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2020-03-19 10:27:34 +0800

    Add ViewCommand stubs for Resume and Internship

---
+++ src/main/java/seedu/address/logic/commands/stubs/ViewResumeCommandStub.java
@@ -0,0 +1,19 @@
+package seedu.address.logic.commands.stubs;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.view.ViewCommand;
+import seedu.address.model.Model;
+
+public class ViewResumeCommandStub extends ViewCommand {
+    private Index targetIndex;
+
+    public ViewResumeCommandStub(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        return null;
+    }
+}
