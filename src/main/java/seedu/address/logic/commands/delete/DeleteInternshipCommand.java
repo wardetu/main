a0ce4de847edac61d23d8d40b7300c9678f29ff4
helloimhai <1503.hai.nguyen@gmail.com>
2020-03-26 13:32:50 +0800

    Fix some stuff in View

--- src/main/java/seedu/address/logic/commands/delete/DeleteInternshipCommand.java
+++ src/main/java/seedu/address/logic/commands/delete/DeleteInternshipCommand.java
@@ -30,7 +30,7 @@ public class DeleteInternshipCommand extends DeleteCommand {
 
         model.deleteInternship(toDelete);
         model.setInternshipToDisplay();
-        
+
         return new CommandResult(toDelete.toString(),
                 String.format(MESSAGE_DELETE_ITEM_SUCCESS, toDelete.getType().getFullType()));
     }
