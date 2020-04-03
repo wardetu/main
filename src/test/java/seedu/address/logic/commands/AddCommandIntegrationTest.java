8a3056f12f82a5452c27ad83b4cf2ef65897359c
helloimhai <1503.hai.nguyen@gmail.com>
2020-04-03 12:55:44 +0800

    Add all of AddCommandIntegrationTest

--- src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
@@ -8,13 +9,22 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.add.AddCommand;
 import seedu.address.logic.commands.add.AddInternshipCommand;
+import seedu.address.logic.commands.add.AddProjectCommand;
+import seedu.address.logic.commands.add.AddResumeCommand;
+import seedu.address.logic.commands.add.AddSkillCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.item.Internship;
+import seedu.address.model.item.Project;
+import seedu.address.model.item.Resume;
+import seedu.address.model.item.Skill;
 import seedu.address.testutil.TypicalInternship;
+import seedu.address.testutil.TypicalProject;
+import seedu.address.testutil.TypicalResume;
 import seedu.address.testutil.TypicalResumeBook;
+import seedu.address.testutil.TypicalSkill;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code AddCommand}.
@@ -53,10 +63,92 @@ public class AddCommandIntegrationTest {
                 new CommandException(AddCommand.MESSAGE_DUPLICATE_ITEM));
     }
 
-    /*@Test
-    public void execute_duplicatePerson_throwsCommandException() {
-        PersonalDetail personInList = model.getResumeBook().getItemToDisplayList().get(0);
-        assertCommandFailure(new AddCommand(personInList), model, AddCommand.MESSAGE_DUPLICATE_PERSON);
+    @Test
+    public void execute_newResume_success() {
+        Resume validResume = TypicalResume.SE_RESUME;
+
+        Model expectedModel = new ModelManager(model.getResumeBook(), new UserPrefs());
+        expectedModel.addResume(validResume);
+        expectedModel.setResumeToDisplay();
+
+        assertCommandSuccess(new AddResumeCommand(validResume),
+                model,
+                new CommandResult(validResume.toString(),
+                        String.format(AddResumeCommand.MESSAGE_SUCCESS,
+                                validResume.getType().getFullType())),
+                expectedModel);
     }
-*/
+
+    @Test
+    public void execute_duplicateResume_throwsCommandException() {
+        Resume validResume = TypicalResume.ME_RESUME;
+
+        assertCommandFailure(new AddResumeCommand(validResume),
+                model,
+                new CommandException(AddCommand.MESSAGE_DUPLICATE_ITEM));
+    }
+
+    @Test
+    public void execute_newSkill_success() {
+        Skill validSkill = TypicalSkill.GIT;
+
+        Model expectedModel = new ModelManager(model.getResumeBook(), new UserPrefs());
+        expectedModel.addSkill(validSkill);
+        expectedModel.setSkillToDisplay();
+
+        assertCommandSuccess(new AddSkillCommand(validSkill),
+                model,
+                new CommandResult(validSkill.toString(),
+                        String.format(AddSkillCommand.MESSAGE_SUCCESS,
+                                validSkill.getType().getFullType())),
+                expectedModel);
+    }
+
+    @Test
+    public void execute_duplicateSkill_throwsCommandException() {
+        Skill validSkill = TypicalSkill.REACT;
+
+        assertCommandFailure(new AddSkillCommand(validSkill),
+                model,
+                new CommandException(AddCommand.MESSAGE_DUPLICATE_ITEM));
+    }
+
+    @Test
+    public void execute_newProject_success() {
+        Project validProject = TypicalProject.DUKE;
+
+        Model expectedModel = new ModelManager(model.getResumeBook(), new UserPrefs());
+        expectedModel.addProject(validProject);
+        expectedModel.setProjectToDisplay();
+
+        assertCommandSuccess(new AddProjectCommand(validProject),
+                model,
+                new CommandResult(validProject.toString(),
+                        String.format(AddProjectCommand.MESSAGE_SUCCESS,
+                                validProject.getType().getFullType())),
+                expectedModel);
+    }
+
+    @Test
+    public void execute_duplicateProject_throwsCommandException() {
+        Project validProject = TypicalProject.ORBITAL;
+
+        assertCommandFailure(new AddProjectCommand(validProject),
+                model,
+                new CommandException(AddCommand.MESSAGE_DUPLICATE_ITEM));
+    }
+
+    @Test
+    public void equals() {
+        Project validProject = TypicalProject.ORBITAL;
+        Skill validSkill = TypicalSkill.REACT;
+        Resume validResume = TypicalResume.ME_RESUME;
+        Internship validInternship = TypicalInternship.GOOGLE;
+        assertNotEquals(validInternship, validProject);
+        assertNotEquals(validSkill, validProject);
+        assertNotEquals(validResume, validProject);
+        assertNotEquals(validSkill, validInternship);
+        assertNotEquals(validResume, validInternship);
+        assertNotEquals(validResume, validSkill);
+    }
 }
