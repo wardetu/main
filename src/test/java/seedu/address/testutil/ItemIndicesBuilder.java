42bbc8b8f3e762747bcce83a0afcf65bf259019b
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2020-04-05 21:02:29 +0800

    Add ItemIndicesBuilder and initialise redit command test

---
+++ src/test/java/seedu/address/testutil/ItemIndicesBuilder.java
@@ -0,0 +1,46 @@
+package seedu.address.testutil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * A builder class to support testing of ResumeEditCommand and TagPullCommand.
+ */
+public class ItemIndicesBuilder {
+    List<Integer> itemIndices;
+
+    public ItemIndicesBuilder() {
+        itemIndices = new ArrayList<>();
+    }
+
+    /**
+     * Initializes the ItemIndicesBuilder with the data of {@code itemIndices}.
+     * @param itemIndices
+     */
+    public ItemIndicesBuilder(List<Integer> itemIndices) {
+        this.itemIndices = itemIndices;
+    }
+
+    /**
+     * Returns the ItemIndicesBuilder which has included index i.
+     * @param i an Integer representing the index of an item.
+     * @return ItemIndicesBuilder with the specified index.
+     */
+    public ItemIndicesBuilder add(Integer i) {
+        itemIndices.add(i);
+        return this;
+    }
+
+    /**
+     * Returns an empty Optional which represents the item prefix not supplied in the argument of the command.
+     * @return An empty Optional.
+     */
+    public static Optional<List<Integer>> empty() {
+        return Optional.empty();
+    }
+
+    public Optional<List<Integer>> build() {
+        return Optional.of(itemIndices);
+    }
+}
