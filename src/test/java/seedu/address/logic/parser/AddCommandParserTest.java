02abdd44f8f7c8db0437a4ae0a0a9c32ae23bdf5
helloimhai <1503.hai.nguyen@gmail.com>
2020-04-07 11:31:23 +0800

    Take out AddCommandParser from lonely package

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.parser.add;
+package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_FROM_DESC;
