d699c5bcc5644b373b514b0e2d65ce83d7ecdf7e
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2020-04-07 15:11:14 +0800

    Add redit command parser test for when all items are specified

---
+++ src/test/java/seedu/address/logic/parser/ResumeEditCommandParserTest.java
@@ -0,0 +1,63 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERNSHIP;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PROJECT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SKILL;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_ITEM;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.ResumeEditCommand;
+import seedu.address.testutil.ItemIndicesBuilder;
+
+public class ResumeEditCommandParserTest {
+    private ResumeEditCommandParser parser = new ResumeEditCommandParser();
+
+    @Test
+    public void parse_allItemsSpecified_success() {
+        ItemIndicesBuilder internshipIndicesBuilder = new ItemIndicesBuilder().add(1).add(3).add(4);
+        ItemIndicesBuilder projectIndicesBuilder = new ItemIndicesBuilder().add(1).add(2);
+        ItemIndicesBuilder skillIndicesBuilder=  new ItemIndicesBuilder().add(1);
+
+        // Standard
+        assertParseSuccess(parser,
+                "1 " + PREFIX_INTERNSHIP + " " + internshipIndicesBuilder.toString() + " " + PREFIX_SKILL +
+                        " " + skillIndicesBuilder.toString() + " " + PREFIX_PROJECT + " " +
+                        projectIndicesBuilder.toString(),
+                new ResumeEditCommand(INDEX_FIRST_ITEM, internshipIndicesBuilder.build(), projectIndicesBuilder.build(),
+                        skillIndicesBuilder.build()));
+
+        // Shuffle the order of prefixes
+        assertParseSuccess(parser,
+                "1 " + PREFIX_PROJECT + " " + projectIndicesBuilder.toString() + " " + PREFIX_INTERNSHIP +
+                        " " + internshipIndicesBuilder.toString() + " " + PREFIX_SKILL + " " +
+                        skillIndicesBuilder.toString(),
+                new ResumeEditCommand(INDEX_FIRST_ITEM, internshipIndicesBuilder.build(), projectIndicesBuilder.build(),
+                        skillIndicesBuilder.build()));
+
+        // multiple project prefixes -- only last one accepted
+        assertParseSuccess(parser,
+                "1 " + PREFIX_PROJECT + " 100 200 " + PREFIX_INTERNSHIP + " " +
+                        internshipIndicesBuilder.toString() + " " + PREFIX_SKILL + " " +
+                        skillIndicesBuilder.toString() + " " + PREFIX_PROJECT + " " + projectIndicesBuilder.toString(),
+                new ResumeEditCommand(INDEX_FIRST_ITEM, internshipIndicesBuilder.build(), projectIndicesBuilder.build(),
+                        skillIndicesBuilder.build()));
+
+        // multiple internship prefixes -- only last one accepted
+        assertParseSuccess(parser,
+                "1 " + PREFIX_INTERNSHIP + " 100 200 " + PREFIX_INTERNSHIP + " " +
+                        internshipIndicesBuilder.toString() + " " + PREFIX_SKILL + " " +
+                        skillIndicesBuilder.toString() + " " + PREFIX_PROJECT + " " + projectIndicesBuilder.toString(),
+                new ResumeEditCommand(INDEX_FIRST_ITEM, internshipIndicesBuilder.build(), projectIndicesBuilder.build(),
+                        skillIndicesBuilder.build()));
+
+        // multiple skill prefixes -- only last one accepted
+        assertParseSuccess(parser,
+                "1 " + PREFIX_SKILL + " 100 200 " + PREFIX_INTERNSHIP + " " +
+                        internshipIndicesBuilder.toString() + " " + PREFIX_SKILL + " " +
+                        skillIndicesBuilder.toString() + " " + PREFIX_PROJECT + " " + projectIndicesBuilder.toString(),
+                new ResumeEditCommand(INDEX_FIRST_ITEM, internshipIndicesBuilder.build(), projectIndicesBuilder.build(),
+                        skillIndicesBuilder.build()));
+    }
+}
