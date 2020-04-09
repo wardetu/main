695b23cf95433006187884e0f90fcad37ce4ac20
helloimhai <1503.hai.nguyen@gmail.com>
2020-04-09 22:40:43 +0800

    Copy in TypicalResumeBook

--- src/test/java/seedu/address/testutil/TypicalResumeBook.java
+++ src/test/java/seedu/address/testutil/TypicalResumeBook.java
@@ -1,6 +1,7 @@
 package seedu.address.testutil;
 
 import seedu.address.model.ResumeBook;
+import seedu.address.model.item.Skill;
 
 /**
  * A utility class containing a list of {@code ResumeBook} objects to be used in tests.
@@ -20,17 +21,17 @@ public class TypicalResumeBook {
             .withResume(TypicalResume.SE_RESUME)
             .build();
     public static final ResumeBook TYPICAL_COPY = new ResumeBookBuilder()
-            .withInternship(TypicalInternship.GOOGLE)
-            .withInternship(TypicalInternship.NINJA_VAN)
-            .withInternship(TypicalInternship.PAYPAL)
+            .withInternship(new InternshipBuilder(TypicalInternship.GOOGLE).build())
+            .withInternship(new InternshipBuilder(TypicalInternship.NINJA_VAN).build())
+            .withInternship(new InternshipBuilder(TypicalInternship.PAYPAL).build())
             .withNote((new NoteBuilder(TypicalNote.FINISH_CS_2103).build()))
-            .withPerson(TypicalPerson.ALICE)
-            .withProject(TypicalProject.ORBITAL)
-            .withProject(TypicalProject.DUKE)
-            .withSkill(TypicalSkill.REACT)
-            .withSkill(TypicalSkill.GIT)
-            .withResume(TypicalResume.ME_RESUME)
-            .withResume(TypicalResume.SE_RESUME)
+            .withPerson(new PersonBuilder(TypicalPerson.ALICE).build())
+            .withProject(new ProjectBuilder(TypicalProject.ORBITAL).build())
+            .withProject(new ProjectBuilder(TypicalProject.DUKE).build())
+            .withSkill(new SkillBuilder(TypicalSkill.REACT).build())
+            .withSkill(new SkillBuilder(TypicalSkill.GIT).build())
+            .withResume(new ResumeBuilder(TypicalResume.ME_RESUME).build())
+            .withResume(new ResumeBuilder(TypicalResume.SE_RESUME).build())
             .build();
     public static final ResumeBook TYPICAL_WITHOUT_GOOGLE = new ResumeBookBuilder()
             .withInternship(TypicalInternship.NINJA_VAN)
@@ -42,13 +43,13 @@ public class TypicalResumeBook {
             .withResume(TypicalResume.ME_RESUME)
             .build();
     public static final ResumeBook TYPICAL_WITHOUT_GOOGLE_COPY = new ResumeBookBuilder()
-            .withInternship(TypicalInternship.NINJA_VAN)
-            .withInternship(TypicalInternship.PAYPAL)
-            .withNote((new NoteBuilder(TypicalNote.FINISH_CS_2103).build()))
-            .withPerson(TypicalPerson.ALICE)
-            .withProject(TypicalProject.ORBITAL)
-            .withSkill(TypicalSkill.REACT)
-            .withResume(TypicalResume.ME_RESUME)
+            .withInternship(new InternshipBuilder(TypicalInternship.NINJA_VAN).build())
+            .withInternship(new InternshipBuilder(TypicalInternship.PAYPAL).build())
+            .withNote(new NoteBuilder(TypicalNote.FINISH_CS_2103).build())
+            .withPerson(new PersonBuilder(TypicalPerson.ALICE).build())
+            .withProject(new ProjectBuilder(TypicalProject.ORBITAL).build())
+            .withSkill(new SkillBuilder(TypicalSkill.REACT).build())
+            .withResume(new ResumeBuilder(TypicalResume.ME_RESUME).build())
             .build();
 
     // This TypicalResumeBook is used mainly for the ResumeEdit and TagPull Command tests
