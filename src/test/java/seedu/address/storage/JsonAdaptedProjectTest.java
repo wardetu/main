02cfb6094b32a3e79d4be90785f6cbed15112553
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2020-04-10 23:06:33 +0800

    Add invalid and missing fields test for JsonAdaptedProjectTest

--- src/test/java/seedu/address/storage/JsonAdaptedProjectTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedProjectTest.java
@@ -1,7 +1,11 @@
 package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_NAME;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_NAME_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_TAG;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_TIME;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_WEBSITE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DESCRIPTION_DUKE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DESCRIPTION_ORBITAL;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PROJECT_NAME_DUKE;
@@ -21,12 +25,15 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.item.field.Description;
 import seedu.address.model.item.field.Name;
+import seedu.address.model.item.field.Time;
+import seedu.address.model.item.field.Website;
+import seedu.address.model.tag.Tag;
 import seedu.address.testutil.ProjectBuilder;
 import seedu.address.testutil.TypicalProject;
 
 public class JsonAdaptedProjectTest {
-    public static final String INVALID_NAME = "&badname";
     @Test
     public void toModelType_validProject_returnsProject() throws IllegalValueException {
         // Without tags
@@ -56,6 +63,35 @@ public class JsonAdaptedProjectTest {
     }
 
     @Test
+    public void toModelType_missingId_throwsIllegalValueException() {
+        JsonAdaptedProject jsonAdaptedProject =
+                new JsonAdaptedProject(VALID_PROJECT_NAME_DUKE, null, VALID_TIME_2, VALID_WEBSITE_DUKE,
+                        VALID_DESCRIPTION_DUKE, new ArrayList<>());
+        assertThrows(IllegalValueException.class,
+                String.format(MISSING_FIELD_MESSAGE_FORMAT, "Id"),
+                jsonAdaptedProject::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidId_throwsIllegalValueException() {
+        // Id must be an integer
+        JsonAdaptedProject jsonAdaptedProject =
+                new JsonAdaptedProject(VALID_PROJECT_NAME_DUKE, "a", VALID_TIME_2, VALID_WEBSITE_DUKE,
+                        VALID_DESCRIPTION_DUKE, new ArrayList<>());
+        assertThrows(IllegalValueException.class,
+                "The id field can only be an integer.",
+                jsonAdaptedProject::toModelType);
+
+        // Id cannot be negative
+        jsonAdaptedProject =
+                new JsonAdaptedProject(VALID_PROJECT_NAME_DUKE, "-1", VALID_TIME_2, VALID_WEBSITE_DUKE,
+                        VALID_DESCRIPTION_DUKE, new ArrayList<>());
+        assertThrows(IllegalValueException.class,
+                "The id field must not be negative.",
+                jsonAdaptedProject::toModelType);
+    }
+
+    @Test
     public void toModelType_missingName_throwsIllegalValueException() {
         JsonAdaptedProject jsonAdaptedProject =
                 new JsonAdaptedProject(null, "1", VALID_TIME_2, VALID_WEBSITE_DUKE, VALID_DESCRIPTION_DUKE,
@@ -72,6 +108,78 @@ public class JsonAdaptedProjectTest {
                         new ArrayList<>());
         assertThrows(IllegalValueException.class,
                 Name.MESSAGE_CONSTRAINTS,
+                jsonAdaptedProject::toModelType);
+    }
+
+    @Test
+    public void toModelType_missingTime_throwsIllegalValueException() {
+        JsonAdaptedProject jsonAdaptedProject =
+                new JsonAdaptedProject(VALID_PROJECT_NAME_DUKE, "1", null, VALID_WEBSITE_DUKE,
+                        VALID_DESCRIPTION_DUKE, new ArrayList<>());
+        assertThrows(IllegalValueException.class,
+                String.format(MISSING_FIELD_MESSAGE_FORMAT, Time.class.getSimpleName()),
+                jsonAdaptedProject::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidTime_throwsIllegalValueException() {
+        JsonAdaptedProject jsonAdaptedProject =
+                new JsonAdaptedProject(VALID_PROJECT_NAME_DUKE, "1", INVALID_TIME, VALID_WEBSITE_DUKE,
+                        VALID_DESCRIPTION_DUKE, new ArrayList<>());
+        assertThrows(IllegalValueException.class,
+                Time.MESSAGE_CONSTRAINTS,
+                jsonAdaptedProject::toModelType);
+    }
+
+    @Test
+    public void toModelType_missingWebsite_throwsIllegalValueException() {
+        JsonAdaptedProject jsonAdaptedProject =
+                new JsonAdaptedProject(VALID_PROJECT_NAME_DUKE, "1", VALID_TIME_2, null,
+                        VALID_DESCRIPTION_DUKE, new ArrayList<>());
+        assertThrows(IllegalValueException.class,
+                String.format(MISSING_FIELD_MESSAGE_FORMAT, Website.class.getSimpleName()),
+                jsonAdaptedProject::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidWebsite_throwsIllegalValueException() {
+        JsonAdaptedProject jsonAdaptedProject =
+                new JsonAdaptedProject(VALID_PROJECT_NAME_DUKE, "1", VALID_TIME_2, INVALID_WEBSITE,
+                        VALID_DESCRIPTION_DUKE, new ArrayList<>());
+        assertThrows(IllegalValueException.class,
+                Website.MESSAGE_CONSTRAINTS,
+                jsonAdaptedProject::toModelType);
+    }
+
+    @Test
+    public void toModelType_missingDescription_throwsIllegalValueException() {
+        JsonAdaptedProject jsonAdaptedProject =
+                new JsonAdaptedProject(VALID_PROJECT_NAME_DUKE, "1", VALID_TIME_2, VALID_WEBSITE_DUKE,
+                        null, new ArrayList<>());
+        assertThrows(IllegalValueException.class,
+                String.format(MISSING_FIELD_MESSAGE_FORMAT, Description.class.getSimpleName()),
+                jsonAdaptedProject::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidDescription_throwsIllegalValueException() {
+        JsonAdaptedProject jsonAdaptedProject =
+                new JsonAdaptedProject(VALID_PROJECT_NAME_DUKE, "1", VALID_TIME_2, VALID_WEBSITE_DUKE,
+                        "", new ArrayList<>());
+        assertThrows(IllegalValueException.class,
+                Description.MESSAGE_CONSTRAINTS,
+                jsonAdaptedProject::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidTags_throwsIllegalValueException() {
+        List<JsonAdaptedTag> tags = new ArrayList<>();
+        tags.add(new JsonAdaptedTag(INVALID_TAG));
+        JsonAdaptedProject jsonAdaptedProject =
+                new JsonAdaptedProject(VALID_PROJECT_NAME_DUKE, "1", VALID_TIME_2, VALID_WEBSITE_DUKE,
+                        VALID_DESCRIPTION_DUKE, tags);
+        assertThrows(IllegalValueException.class,
+                Tag.MESSAGE_CONSTRAINTS,
                 jsonAdaptedProject::toModelType);
     }
 }
