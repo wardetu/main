0fb961252a3e6c3a6b33b962a552aa47ae4a4ee7
duongphammmm <46336643+duongphammmm@users.noreply.github.com>
2020-04-10 22:50:43 +0800

    Fix error message

--- src/main/java/seedu/address/model/item/field/DisplayPicture.java
+++ src/main/java/seedu/address/model/item/field/DisplayPicture.java
@@ -9,7 +9,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class DisplayPicture {
     public static final String MESSAGE_CONSTRAINTS_VALID_PATH = "Display picture path has to be a valid path of the "
             + "image you want to display as your profile picture. Example format: /Users/nhamquochung/Desktop/test.png";
-    public static final String MESSAGE_CONSTRAINTS_FILE_TYPE = "Display picture path has to be a valid image file "
+    public static final String MESSAGE_CONSTRAINTS_FILE_TYPE = "Display picture has to be a valid image file "
             + "ending with .jpg, .png or .jpeg. Example format: /Users/nhamquochung/Desktop/test.png";
 
     /**
