3765ae9d69e5aad072eab3ac31fc13e60ae932e7
helloimhai <1503.hai.nguyen@gmail.com>
2020-04-11 15:58:55 +0800

    Finish testing me commands

--- src/test/java/seedu/address/logic/commands/EditUserCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditUserCommandTest.java
@@ -2,22 +2,23 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.VALID_GITHUB_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.edit.EditUserCommand.MESSAGE_EDIT_PERSON_SUCCESS;
+import static seedu.address.logic.commands.edit.EditUserCommand.MESSAGE_FROM_TO_MISORDER;
+import static seedu.address.logic.commands.edit.EditUserCommand.USER_NOT_EDITED;
 import static seedu.address.testutil.TypicalPerson.AMY;
 import static seedu.address.testutil.TypicalPerson.BOB;
 
-import javafx.scene.AmbientLight;
 import org.junit.jupiter.api.BeforeEach;
 
 import org.junit.jupiter.api.Test;
 import seedu.address.logic.commands.edit.EditUserCommand;
-import seedu.address.logic.commands.edit.EditUserDescriptor;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.commands.results.EditUserCommandResult;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.item.Person;
 import seedu.address.testutil.EditUserDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.ResumeBookBuilder;
@@ -40,7 +41,7 @@ public class EditUserCommandTest {
     }
 
     @Test
-    public void execute_editAllFields_success() {
+    public void execute_editMultipleFields_success() {
         expectedModel.setUser(new PersonBuilder(BOB).build());
         EditUserCommand editUserCommand = new EditUserCommand(new EditUserDescriptorBuilder(BOB).build());
         assertCommandSuccess(editUserCommand, model,
@@ -48,28 +49,46 @@ public class EditUserCommandTest {
                 expectedModel);
     }
 
-//    @Test
-//    public void execute_editSingleField_success() {
-//        // Edit only name
-//        expectedModel.setUser(new PersonBuilder(AMY).withName(VALID_NAME_BOB).build());
-//        EditUserCommand editUserCommand = new EditUserCommand(
-//                new EditUserDescriptorBuilder(AMY).withName(VALID_NAME_BOB).build());
-//        assertCommandSuccess(editUserCommand, model,
-//                new EditUserCommandResult(
-//                        new PersonBuilder(AMY).withName(VALID_NAME_BOB).build().toString(),
-//                        MESSAGE_EDIT_PERSON_SUCCESS,
-//                        expectedModel.getDisplayType()),
-//                expectedModel);
-//
-//        // Edit only Github
-//        expectedModel.setUser(new PersonBuilder(AMY).withGithub(VALID_GITHUB_BOB).build());
-//        editUserCommand = new EditUserCommand(
-//                new EditUserDescriptorBuilder(AMY).withGithub(VALID_GITHUB_BOB).build());
-//        assertCommandSuccess(editUserCommand, model,
-//                new EditUserCommandResult(
-//                        new PersonBuilder(AMY).withGithub(VALID_GITHUB_BOB).build().toString(),
-//                        MESSAGE_EDIT_PERSON_SUCCESS,
-//                        expectedModel.getDisplayType()),
-//                expectedModel);
-//    }
+    @Test
+    public void execute_editSingleFieldName_success() {
+        expectedModel.setUser(new PersonBuilder(AMY).withName(VALID_NAME_BOB).build());
+        EditUserCommand editUserCommand = new EditUserCommand(
+                new EditUserDescriptorBuilder().withName(VALID_NAME_BOB).build());
+        assertCommandSuccess(editUserCommand, model,
+                new EditUserCommandResult(
+                        new PersonBuilder(AMY).withName(VALID_NAME_BOB).build().toString(),
+                        MESSAGE_EDIT_PERSON_SUCCESS,
+                        expectedModel.getDisplayType()),
+                expectedModel);
+    }
+
+    @Test
+    public void execute_editSingleFieldGithub_success() {
+        expectedModel.setUser(new PersonBuilder(AMY).withGithub(VALID_GITHUB_BOB).build());
+        EditUserCommand editUserCommand = new EditUserCommand(
+                new EditUserDescriptorBuilder().withGithub(VALID_GITHUB_BOB).build());
+        assertCommandSuccess(editUserCommand, model,
+                new EditUserCommandResult(
+                        new PersonBuilder(AMY).withGithub(VALID_GITHUB_BOB).build().toString(),
+                        MESSAGE_EDIT_PERSON_SUCCESS,
+                        expectedModel.getDisplayType()),
+                expectedModel);
+    }
+
+    @Test
+    public void execute_editNoFields_failure() {
+        EditUserCommand editUserCommand = new EditUserCommand(new EditUserDescriptorBuilder().build());
+        assertCommandFailure(editUserCommand,
+                model,
+                new CommandException(USER_NOT_EDITED));
+    }
+
+    @Test
+    public void execute_fromFieldLaterThanTo_failure() {
+        EditUserCommand editUserCommand = new EditUserCommand(
+                new EditUserDescriptorBuilder().withFrom("10-2030").build());
+        assertCommandFailure(editUserCommand,
+                model,
+                new CommandException(MESSAGE_FROM_TO_MISORDER));
+    }
 }
