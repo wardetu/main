3b73aace7351619c8d0f4c3d824b63feabb577c3
helloimhai <1503.hai.nguyen@gmail.com>
2020-04-13 01:47:47 +0800

    Initial stages of setting up ModelManagerTest

--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -1,25 +1,33 @@
 package seedu.address.model;
-/*
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_ITEMS;
-import static seedu.address.testutil.Assert.assertThrows;*/
-/*import static seedu.address.testutil.TypicalPersonalDetails.ALICE;
-import static seedu.address.testutil.TypicalPersonalDetails.BENSON;*/
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_TECH;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalInternship.GOOGLE;
+import static seedu.address.testutil.TypicalInternship.NINJA_VAN;
+import static seedu.address.testutil.TypicalInternship.PAYPAL;
 
-/*import java.nio.file.Path;
+
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.GuiSettings;
-import seedu.address.model.item.field.NameContainsKeywordsPredicate;
-import seedu.address.testutil.AddressBookBuilder;*/
+import seedu.address.model.item.Internship;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.util.ItemUtil;
+import seedu.address.testutil.InternshipBuilder;
+import seedu.address.testutil.PersonBuilder;
 
 public class ModelManagerTest {
-/*
+
     private ModelManager modelManager = new ModelManager();
 
     @Test
@@ -37,14 +45,14 @@ public class ModelManagerTest {
     @Test
     public void setUserPrefs_validUserPrefs_copiesUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
-        userPrefs.setResumeBookFilePath(Paths.get("address/book/file/path"));
+        userPrefs.setResumeBookFilePath(Paths.get("resume/book/file/path"));
         userPrefs.setGuiSettings(new GuiSettings(1, 2, 3, 4));
         modelManager.setUserPrefs(userPrefs);
         assertEquals(userPrefs, modelManager.getUserPrefs());
 
         // Modifying userPrefs should not modify modelManager's userPrefs
         UserPrefs oldUserPrefs = new UserPrefs(userPrefs);
-        userPrefs.setResumeBookFilePath(Paths.get("new/address/book/file/path"));
+        userPrefs.setResumeBookFilePath(Paths.get("new/resume/book/file/path"));
         assertEquals(oldUserPrefs, modelManager.getUserPrefs());
     }
 
@@ -61,74 +69,118 @@ public class ModelManagerTest {
     }
 
     @Test
-    public void setAddressBookFilePath_nullPath_throwsNullPointerException() {
+    public void setResumeBookFilePath_nullPath_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> modelManager.setResumeBookFilePath(null));
     }
 
     @Test
-    public void setAddressBookFilePath_validPath_setsAddressBookFilePath() {
-        Path path = Paths.get("address/book/file/path");
+    public void setResumeBookFilePath_validPath_setsResumeBookFilePath() {
+        Path path = Paths.get("resume/book/file/path");
         modelManager.setResumeBookFilePath(path);
         assertEquals(path, modelManager.getResumeBookFilePath());
     }
 
+    // Internship-related tests
+
     @Test
-    public void hasPerson_nullPerson_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> modelManager.hasPersonalDetail(null));
+    public void hasInternship_nullInternship_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.hasInternship(null));
     }
 
     @Test
-    public void hasPerson_personNotInAddressBook_returnsFalse() {
-        assertFalse(modelManager.hasPersonalDetail(ALICE));
+    public void hasInternship_internshipNotInResumeBook_returnsFalse() {
+        assertFalse(modelManager.hasInternship(new InternshipBuilder(GOOGLE).build()));
+
+        // add then delete
+        modelManager.addInternship(new InternshipBuilder(GOOGLE).build());
+        modelManager.deleteInternship(new InternshipBuilder(GOOGLE).build());
+        assertFalse(modelManager.hasInternship(new InternshipBuilder(GOOGLE).build()));
+
+        // add then set
+        modelManager.addInternship(new InternshipBuilder(GOOGLE).build());
+        modelManager.setInternship(new InternshipBuilder(GOOGLE).build(), PAYPAL);
+        assertFalse(modelManager.hasInternship(new InternshipBuilder(GOOGLE).build()));
     }
 
     @Test
-    public void hasPerson_personInAddressBook_returnsTrue() {
-        modelManager.addPersonalDetail(ALICE);
-        assertTrue(modelManager.hasPersonalDetail(ALICE));
+    public void hasInternship_internshipInResumeBook_returnsTrue() {
+        modelManager.addInternship(new InternshipBuilder(GOOGLE).build());
+        assertTrue(modelManager.hasInternship(new InternshipBuilder(GOOGLE).build()));
     }
 
     @Test
-    public void getFilteredPersonList_modifyList_throwsUnsupportedOperationException() {
-        assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredPersonalDetailList().remove(0));
+    public void getInternshipSize_emptyResumeBook_returnsZero() {
+        assertEquals(0, modelManager.getInternshipSize());
     }
 
     @Test
-    public void equals() {
-        ResumeBook addressBook = new AddressBookBuilder().withPerson(ALICE).withPerson(BENSON).build();
-        ResumeBook differentAddressBook = new ResumeBook();
-        UserPrefs userPrefs = new UserPrefs();
-
-        // same values -> returns true
-        modelManager = new ModelManager(addressBook, userPrefs);
-        ModelManager modelManagerCopy = new ModelManager(addressBook, userPrefs);
-        assertTrue(modelManager.equals(modelManagerCopy));
-
-        // same object -> returns true
-        assertTrue(modelManager.equals(modelManager));
-
-        // null -> returns false
-        assertFalse(modelManager.equals(null));
-
-        // different types -> returns false
-        assertFalse(modelManager.equals(5));
-
-        // different addressBook -> returns false
-        assertFalse(modelManager.equals(new ModelManager(differentAddressBook, userPrefs)));
-
-        // different filteredList -> returns false
-        String[] keywords = ALICE.getName().fullName.split("\\s+");
-        modelManager.updateFilteredPersonalDetailList(new NameContainsKeywordsPredicate(Arrays.asList(keywords)));
-        assertFalse(modelManager.equals(new ModelManager(addressBook, userPrefs)));
-
-        // resets modelManager to initial state for upcoming tests
-        modelManager.updateFilteredPersonalDetailList(PREDICATE_SHOW_ALL_ITEMS);
-
-        // different userPrefs -> returns false
-        UserPrefs differentUserPrefs = new UserPrefs();
-        differentUserPrefs.setResumeBookFilePath(Paths.get("differentFilePath"));
-        assertFalse(modelManager.equals(new ModelManager(addressBook, differentUserPrefs)));
+    public void getInternshipByTag_emptyResumeBook_returnsEmptyList() {
+        assertEquals(new ArrayList<Internship>(), modelManager.getInternshipsByTag(new Tag(VALID_TAG_TECH)));
     }
 
- */
+    @Test
+    public void setInternshipsToDisplay_internshipInResumeBook_success() {
+        modelManager.addInternship(new InternshipBuilder(GOOGLE).build());
+        modelManager.addInternship(new InternshipBuilder(PAYPAL).build());
+        modelManager.setInternshipToDisplay();
+        assertEquals(modelManager.getDisplayType(), ItemUtil.INTERNSHIP_ALIAS);
+        assertEquals(Arrays.asList(GOOGLE, PAYPAL), modelManager.getFilteredItemList());
+    }
+
+    @Test
+    public void sortInternships_internshipInResumeBook_success() {
+        modelManager.addInternship(new InternshipBuilder(GOOGLE).build());
+        modelManager.addInternship(new InternshipBuilder(PAYPAL).build());
+        modelManager.setInternshipToDisplay();
+        assertEquals(modelManager.getDisplayType(), ItemUtil.INTERNSHIP_ALIAS);
+        assertEquals(Arrays.asList(GOOGLE, PAYPAL), modelManager.getFilteredItemList());
+    }
+
+//    @Test
+//    public void deletePerson_personIsSelectedAndFirstPersonInFilteredPersonList_selectionCleared() {
+//        modelManager.addInternship(new InternshipBuilder(GOOGLE).build());
+//        modelManager.setSelected(ALICE);
+//        modelManager.deletePerson(ALICE);
+//        assertEquals(null, modelManager.getSelectedPerson());
+//    }
+//
+//    @Test
+//    public void deletePerson_personIsSelectedAndSecondPersonInFilteredPersonList_firstPersonSelected() {
+//        modelManager.addPerson(ALICE);
+//        modelManager.addPerson(BOB);
+//        assertEquals(Arrays.asList(ALICE, BOB), modelManager.getFilteredPersonList());
+//        modelManager.setSelectedPerson(BOB);
+//        modelManager.deletePerson(BOB);
+//        assertEquals(ALICE, modelManager.getSelectedPerson());
+//    }
+//
+//    @Test
+//    public void setPerson_personIsSelected_selectedPersonUpdated() {
+//        modelManager.addPerson(ALICE);
+//        modelManager.setSelectedPerson(ALICE);
+//        Person updatedAlice = new PersonBuilder(ALICE).withEmail(VALID_EMAIL_BOB).build();
+//        modelManager.setPerson(ALICE, updatedAlice);
+//        assertEquals(updatedAlice, modelManager.getSelectedPerson());
+//    }
+//
+//    @Test
+//    public void getFilteredPersonList_modifyList_throwsUnsupportedOperationException() {
+//        assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredPersonList().remove(0));
+//    }
+//
+//    @Test
+//    public void setSelectedPerson_personNotInFilteredPersonList_throwsPersonNotFoundException() {
+//        assertThrows(PersonNotFoundException.class, () -> modelManager.setSelectedPerson(ALICE));
+//    }
+//
+//    @Test
+//    public void setSelectedPerson_personInFilteredPersonList_setsSelectedPerson() {
+//        modelManager.addPerson(ALICE);
+//        assertEquals(Collections.singletonList(ALICE), modelManager.getFilteredPersonList());
+//        modelManager.setSelectedPerson(ALICE);
+//        assertEquals(ALICE, modelManager.getSelectedPerson());
+//    }
+
+
+
 }
